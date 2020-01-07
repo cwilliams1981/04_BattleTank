043c3eb44c80acd5828350cff94dd0279cfc286b
Chuck Williams <chuckwilliams1981@gmail.com>
2020-01-06 21:58:47 -0500

    BattleTank 0.00.05.02 - CrossProduct() Vector Operator

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -15,9 +15,12 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	// auto TankName = GetOwner()->GetName();
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
-	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveForward(ForwardThrow);
+
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
 	// UE_LOG(LogTemp, Warning, TEXT("%s moving to: %s"), *TankName, *AIForwardIntention.ToString())
 }
 
